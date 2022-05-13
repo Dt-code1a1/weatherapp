bd70c33e7456fa64aa661af1d888347e28c4d718
Dt-code1a1 <danielt1113@gmail.com>
2022-05-13 14:37:38 +0200

    Added fading in/out and switchig views

--- scripts/main.js
+++ scripts/main.js
@@ -28,6 +28,7 @@ const connectHTMLElems = () =>{
 const setupListener = () =>{
     viewElems.searchInput.addEventListener('keydown', onEnterSubmit)
     viewElems.searchButton.addEventListener('click', onClickSubmit)
+    viewElems.returnToSearchBtn.addEventListener('click', returnToSearch)
 }
 
 const initializeApp = () =>{
@@ -37,12 +38,52 @@ const initializeApp = () =>{
 
 const onEnterSubmit = event =>{
     if(event.key === 'Enter'){
+        fadeInOut()
         let qurey = viewElems.searchInput.value
         getWeatherByCity(qurey).then(data => {
             console.log(data)
+            switchView()
+            fadeInOut()
         })
     }
 }
-const onClickSubmit = () =>{}
+
+const onClickSubmit = () =>{
+    fadeInOut()
+    let qurey = viewElems.searchInput.value
+        getWeatherByCity(qurey).then(data => {
+            console.log(data)
+            switchView()
+            fadeInOut()
+        })
+    
+}
+
+ const fadeInOut = () => {
+    if (viewElems.mainContainer.style.opacity === '1' || viewElems.mainContainer.style.opacity === '') {
+      viewElems.mainContainer.style.opacity = '0';
+    } else {
+      viewElems.mainContainer.style.opacity = '1';
+    }
+  }
+
+  const switchView = () => {
+    if (viewElems.weatherSearchView.style.display !== 'none') {
+      viewElems.weatherSearchView.style.display = 'none';
+      viewElems.weatherForecastView.style.display = 'block';
+    } else {
+      viewElems.weatherForecastView.style.display = 'none';
+      viewElems.weatherSearchView.style.display = 'flex';
+    }
+  }
+
+  const returnToSearch = () => {
+   fadeInOut();
+
+    setTimeout(() => {
+      switchView();
+      fadeInOut();
+    }, 500);
+  }
 
 document.addEventListener("DOMContentLoaded", initializeApp)
